34eaea9d7d7a5844b2c5993e49eb7686095857a0
MostafaAE <mostafa.aymanae@gmail.com>
2023-08-11 02:48:36 +0300

    Implement add menu items to cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,19 +1,49 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
-  return (
-    <li className='flex gap-4 py-2'>
-      <img src={imageUrl} alt={name}  className={`h-24 ${soldOut ? 'grayscale opacity-70' : ''}`}/>
-      <div className='flex flex-col flex-grow pt-0.5'>
-        <p className='font-medium'>{name}</p>
-        <p className='italic text-sm text-stone-500 capitalize'>{ingredients.join(', ')}</p>
-        <div className='mt-auto flex items-center justify-between'>
-          {!soldOut ? <><p className='text-sm'>{formatCurrency(unitPrice)}</p><Button type={'small'}>Add to cart</Button></> : <p className='text-sm uppercase font-medium text-stone-500'>Sold out</p>}
+  const dispatch = useDispatch();
 
-          
+  function handleAddToCart() {
+    const pizzaItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice,
+    };
+    dispatch(addItem(pizzaItem));
+  }
+
+  return (
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+      />
+      <div className="flex flex-grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(', ')}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <>
+              <p className="text-sm">{formatCurrency(unitPrice)}</p>
+              <Button type={'small'} onClick={handleAddToCart}>
+                Add to cart
+              </Button>
+            </>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
         </div>
       </div>
     </li>
