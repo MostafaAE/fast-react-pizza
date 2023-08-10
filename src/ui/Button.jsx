5e52e6ea5ad8bae9a766c46626729ee13ab9615a
MostafaAE <mostafa.aymanae@gmail.com>
2023-08-10 03:22:19 +0300

    Style cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
-    const base= 'focus:ring- inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
-    const styles={
-      
-      primary: base + ' px-4 py-3 md:px-6 md:py-4',
-      small: base + ' text-xs px-4 py-2 md:px-5 md:py-2.5',
-    }
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+
+    secondary:
+      'inline-block rounded-full font-semibold uppercase tracking-wide px-4 py-2.5 md:px-6 md:py-3.5 border-2 border-stone-300 text-stone-400 focus:ring-stone-200 focus:bg-stone-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed',
+
+    small: base + ' text-xs px-4 py-2 md:px-5 md:py-2.5',
+  };
 
   if (to)
     return (
